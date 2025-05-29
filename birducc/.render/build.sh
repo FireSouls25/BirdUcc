ac3f70beb12ca12dbc08cf42279242056a8454a4
manuel4320 <manuel.viverosreal@campusucc.edu.co>
2025-05-29 15:52:40 -0500

    correcciones

--- birducc/.render/build.sh
+++ birducc/.render/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+set -e  # Detiene el script si ocurre un error
+
+echo "=== Entrando al directorio del proyecto ==="
+cd birducc
+
 echo "=== Iniciando build con Maven ==="
-mvn clean package
+./mvnw clean package -DskipTests
 
 echo "=== Verificando versión de Java ==="
 java -version
+
+echo "=== Ejecutando la aplicación ==="
+java -jar target/*.jar
