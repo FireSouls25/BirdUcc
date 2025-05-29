f2e3adc362644bf83cfd6d54216696350f646a63
manuel4320 <manuel.viverosreal@campusucc.edu.co>
2025-05-29 13:58:27 -0500

    build.sh

--- .render/build.sh
+++ .render/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Da permisos de ejecución al wrapper de Maven
+chmod +x ./mvnw
+
+# Usa Java 17 proporcionado por Render
 export JAVA_HOME=$JAVA17_HOME
+
+# Ejecuta la build
 ./mvnw clean package
